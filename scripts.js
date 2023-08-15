c4331b153e39b93fad7bf680de835a00d8f7bc01
weizhixie <wx02697@yahoo.co.uk>
2023-08-15 21:56:13 +0100

    Add a button along with corresponding functions that allow for changing the number of squares.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
+const GIRD_TOTAL_SIZE = 560;
 const squareContainer = document.querySelector(".square-container");
 
-function createSquares() {
-    for (let i = 0; i < 256; i++) {
+function createSquares(size = 16) {
+    for (let i = 0; i < (size * size); i++) {
         const squares = document.createElement("div");
         squares.classList.add("squares");
         squareContainer.appendChild(squares);
     }
+    changeSquareColor();
 }
 
 function changeSquareColor() {
@@ -17,5 +19,39 @@ function changeSquareColor() {
     });
 }
 
+function changeNumberOfSquares() {
+    const changeSizeBtn = document.querySelector(".change-size-btn");
+    changeSizeBtn.addEventListener("click", () => {
+        const inputSize = prompt("Please enter a grid size between number 1 to 64.");
+        if (inputSize && inputSize > 0 && inputSize <= 64) {
+            removeSquares();
+            createSquares(inputSize);
+            editSquareSize(inputSize);
+        }
+        else {
+            alert("Please enter a grid size between number 1 to 64.")
+        }
+    });
+}
+
+function removeSquares() {
+    const squares = document.querySelectorAll(".squares");
+    squares.forEach((square) => {
+        squareContainer.removeChild(square);
+    });
+}
+
+function editSquareSize(size) {
+    /**
+     * Calculate the percentage of each part(square) of 
+     * the grid total size(total width and total height).
+     */
+    const percentage = GIRD_TOTAL_SIZE / size / GIRD_TOTAL_SIZE * 100;
+    const squares = document.querySelectorAll(".squares");
+    squares.forEach((square) => {
+        square.setAttribute("style", `width: ${percentage}%; height: ${percentage}%;`);
+    });
+}
+
 createSquares();
-changeSquareColor();
+changeNumberOfSquares();
